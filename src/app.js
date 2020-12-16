e17ea73861d69c7d041b59cca2636d41c51c96d0
xavieur <one@email.com>
2020-12-16 11:09:39 +0100

    feat: babel

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
 import sumar, {substraction, multiply, division} from './app-2.js'
 
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 console.log('hola desde app.js')
 console.log(substraction(2,3))
+
+const Parrafo = () => {
+    return <p>¿qué tal?</p>
+}
+
+ReactDOM.render(<Parrafo />, document.querySelector('#appRoot'))
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,25 @@ const path = require('path')
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, 'public','js'),
+        path: path.join(__dirname, 'public', 'js'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        open: true
     }
 }
